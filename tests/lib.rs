01f4b11ab0a6066651ee504cc03838153e5eeb36
jprochazk <pjanv42@gmail.com>
2024-09-09 18:38:11 +0200

    add sintel test

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,13 +3,15 @@ use std::path::Path;
 fn read_sample_file(path: &str) {
     const BASE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/samples");
     let bytes = std::fs::read(Path::new(BASE).join(path)).unwrap();
-    let mp4 = mp4::read(&bytes).unwrap();
-    let samples = &mp4.tracks().get(&1).unwrap().data;
-    std::fs::write(Path::new(BASE).join(format!("{}.bin", path)), samples).unwrap();
-    panic!();
+    mp4::read(&bytes).unwrap();
 }
 
 #[test]
 fn bbb_video_av1_frag() {
     read_sample_file("bbb_video_av1_frag.mp4");
+}
+
+#[test]
+fn sintel_trailer_720p_h264() {
+    read_sample_file("sintel_trailer-720p-h264.mp4");
 }
