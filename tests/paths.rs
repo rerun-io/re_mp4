7989d7bc026f4fbca0d9cb99e14ba31840b33a6f
Andreas Reich <r_andreas2@web.de>
2024-09-30 18:02:02 +0200

    Add simple test for parsing mp4 with multiple tracks

---
+++ tests/paths.rs
@@ -0,0 +1,4 @@
+#[allow(dead_code)]
+
+pub const TEST_BASE_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests");
+pub const SAMPLE_BASE_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/samples");
