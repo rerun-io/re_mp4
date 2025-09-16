a3e41e6f338c6e81f1b338edf926969e6dd9eb93
ntjohnson1 <24689722+ntjohnson1@users.noreply.github.com>
2025-09-16 08:01:23 -0400

    Fix while loop condition and reverse if branch when only single sample_size

--- src/reader.rs
+++ src/reader.rs
@@ -166,7 +166,7 @@ impl Mp4 {
             let stts = &stbl.stts;
 
             // Could probably just always use sample count
-            while (sample_n < stsz.sample_sizes.len() && sample_n == 0)
+            while (sample_n < stsz.sample_sizes.len() && stsz.sample_size == 0)
                 || sample_n < stsz.sample_count as usize
             {
                 // compute offset
@@ -211,10 +211,10 @@ impl Mp4 {
                 }
 
                 let timescale = trak.mdia.mdhd.timescale as u64;
-                let size = if stsz.sample_size != 0 {
-                    stsz.sample_size as u64
-                } else {
+                let size = if stsz.sample_size == 0 {
                     stsz.sample_sizes[sample_n] as u64
+                } else {
+                    stsz.sample_size as u64
                 };
                 let offset = get_sample_chunk_offset(stbl, chunk_index) + offset_in_chunk;
                 offset_in_chunk += size;
